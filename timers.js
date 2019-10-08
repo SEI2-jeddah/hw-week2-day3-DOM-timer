b38776345274f78380e2f157d70c9d8eee2474be
Ibtis0001 <ebt.0101@gmail.com>
2019-10-08 19:25:15 +0300

    week2-4

--- timers.js
+++ timers.js
@@ -1,3 +1,31 @@
+let reset = document.getElementById('reset')
+let start = document.getElementById('start')
+let pause = document.getElementById('pause')
+
+reset.addEventListener('click', funreset)
+start.addEventListener('click', funstart)
+pause.addEventListener('click', funpause)
+
+let seconds = 0
+let timerId = document.getElementById('timer')
+let update;
+function funstart(){
+    update = setInterval(function() {
+        timerId.innerText = "Time Elapsed " + seconds
+             seconds++
+     }, 1000);
+
+}
+
+function funreset(){
+timerId.innerText = "Stop Watch"
+clearInterval(update);
+seconds=0;
+}
+
+function funpause(){
+    clearInterval(update);
+}
 
 
 
