eee34f9ca13d23dc0c459ac865fd7f1a0e0f02d9
Ahlam Fallatah <f18.ahlam@gmail.com>
2019-10-08 23:06:51 +0300

    start timer coding

--- timers.js
+++ timers.js
@@ -1,4 +1,28 @@
 
+var timer = document.querySelector("#timer")
+var controls = document.querySelector(".controls")
+var reset = document.querySelector("#reset");
+var start = document.querySelector("#start");
+var pause = document.querySelector("#pause");
 
+reset.addEventListener("click", "reset");
+start.addEventListener("click", "start");
+pause.addEventListener("click", "pause");
 
+var seconds;
+var timerId;
 
+function updateTime(){
+    timer.innerHTML = ("Time Elapsed: " + seconds)
+}
+function start(){
+    timer.innerHTML = ("Time Elapsed: " + seconds)
+    timeId = setInterval(updatetime, 1000)
+}
+function pause(){
+    clearInterval(timeId)
+}
+function reset(){
+    clearInterval(timeId)
+    timer.innerHTML = ("Stop Watch")
+}
