32f7270d2958d56a1da591fb0808ba985d7bcf56
Doaa Alalawi <Doaa.alalawi@outlook.sa>
2019-10-08 23:46:10 +0300

    done

--- timers.js
+++ timers.js
@@ -1,4 +1,25 @@
+let seconds = 0 
+let timerId
 
+let  head = document.getElementById("timer")
+let startButton = document.getElementById("start").addEventListener("click",updateTime)
+let resetButton = document.getElementById("reset").addEventListener("click",reset)
+let pauseButton = document.getElementById("pause").addEventListener("click",pause)
 
+function updateTime ()
+{
+    timerId = setInterval(function (){
+     seconds ++
+        head.innerText = "Time elapsed: " + seconds
+    },1000)
+}
 
+function pause (){
+    clearInterval(timerId)
+}
 
+function reset(){
+    head.innerText = "Stop Watch"
+    seconds = 0 
+    clearInterval(timerId)
+}
