9b840c0bd514aa22783a6dccb395a70a014765c5
Ali-hd <ali.hamidaddin1@gmail.com>
2019-10-08 21:36:39 +0300

    solved hw

--- timers.js
+++ timers.js
@@ -1,4 +1,29 @@
+reset = document.getElementById("reset");
+start = document.getElementById("start");
+pause = document.getElementById("pause");
 
+timer = document.getElementById("timer");
+let seconds = 0;
+let timerId = "";
+
+reset.onclick = function(){
+    clearInterval(timerId);
+    timer.innerHTML = "Stop Watch"
+    seconds = 0;
+};
+
+
+start.onclick = function updatetime(){
+     timerId = setInterval(function(){
+        seconds++;
+        timer.innerHTML = "Time elapsed: " + seconds;
+    },1000);
+    
+};
+
+pause.onclick = function(){
+    clearInterval(timerId);
+};
 
 
 
