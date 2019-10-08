79f10423804a74d80e26da0fd6556dcbcad55e68
Suhaib Binhimd <sbinhimd@hotmail.com>
2019-10-09 01:12:48 +0300

    timer solved

--- timers.js
+++ timers.js
@@ -1,4 +1,25 @@
+document.getElementById("reset").addEventListener("click", restartTime);
+document.getElementById("start").addEventListener("click", updateTime);
+document.getElementById("pause").addEventListener("click", pauseTime);
 
+var seconds = 0,
+  timerId,
+  started = false;
+var time = document.querySelector("h1");
 
+function updateTime() {
+  timerId = setInterval(function() {
+    seconds += 1;
+    time.innerText = "Time elapsed: " + seconds;
+  }, 1000);
+}
 
+function pauseTime() {
+  clearInterval(timerId);
+}
 
+function restartTime() {
+  seconds = 0;
+  time.innerHTML = "Stop Watch";
+  clearInterval(timerId);
+}
