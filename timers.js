d8e3f5a7bc8b3e6db14038ebf4e1c95a98e55460
AnasShar <anas10895@gmail.com>
2019-10-08 22:53:11 +0300

    Anas

--- timers.js
+++ timers.js
@@ -1,4 +1,42 @@
 
+reset.addEventListener('click', Wreset)
+start.addEventListener('click', Wstart)
+pause.addEventListener('click', Wpause)
+let mint = 0;
+let seconds = 0;
+let timer = document.getElementById('timer')
+let time;
+function Wstart() {
+  time = setInterval(function () {
+
+    for (i = 0; i < 60; i++) {
+      if (seconds > 60) {
+        mint++
+        seconds = 0;
+        timer.innerText = "Seconds  :" + mint 
+        seconds++
+      }
+      else {
+        
+        timer.innerText = "Seconds : " + mint 
+        seconds++
+      }
+
+    }
+
+  }, 1000);
+
+}
+
+function Wreset() {
+  timer.innerText = "Stop Watch"
+  clearInterval(time);
+  mint = 0;
 
 
+}
+
+function Wpause() {
+  clearInterval(time);
+}
 
