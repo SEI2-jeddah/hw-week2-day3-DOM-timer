98326f2f094a47b59b44fcd36466848322d81a81
aishamu <aishamutip@hotmail.com>
2019-10-08 19:46:22 +0300

    Done

--- timers.js
+++ timers.js
@@ -1,4 +1,33 @@
 
 
+var timerId=0;
+var intervalID;
+
+let resetBtn = document.getElementById("reset").onclick= function(){
+    timerId = 0
+
+    clearInterval(intervalID);
+    timer.innerText="Stop Watch";
+}
 
 
+let startBtn = document.getElementById("start").onclick= function(){
+   
+
+
+    intervalID=setInterval(function(){
+ 
+       timer.innerText="Time elapsed:" +timerId
+       timerId++
+    } ,1000)
+
+
+
+ }
+
+
+ let pauseBtn = document.getElementById("pause").onclick= function(){
+    clearInterval(intervalID);
+ }
+
+ 
