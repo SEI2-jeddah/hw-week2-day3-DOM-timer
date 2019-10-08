36cb421d730ebbc2047ab6030ae1a14c74d9dc17
MSalah73 <ms24@pdx.edu>
2019-10-09 00:39:39 +0300

    Mohammed Salah

--- timers.js
+++ timers.js
@@ -1,4 +1,22 @@
+let $start = document.getElementById("start");
+let $pause = document.getElementById("pause");
+let $reset = document.getElementById("reset");
+let $timer = document.getElementById("timer");
 
+var intervalID;
+var seconds = 0;
 
+$start.addEventListener("click", () =>{
+	intervalID = setInterval(() =>{
+		$timer.innerText = "Time elapsed: "+ seconds++;
+}, 1000)})
 
+$pause.addEventListener("click", () =>{
+	clearInterval(intervalID);
+});
 
+$reset.addEventListener("click", () =>{
+	seconds = 0;
+	$timer.innerText = "Stop Watch";
+	clearInterval(intervalID);
+})
