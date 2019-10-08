1f3abe6f2eae289c07523d38bd242bfbd82ee7c9
Yazeed aljuhani <yazeedmalmarwani@gmail.com>
2019-10-08 20:44:14 +0300

    made a stop watch

--- timers.js
+++ timers.js
@@ -1,4 +1,18 @@
-
-
-
-
+var sec = -1
+var starting = 0
+function start(){
+   starting = 1  
+}
+setInterval(function(){
+   let $digitalClock = document.querySelector('#timer') 
+    if(starting){
+       sec++ 
+   $digitalClock.innerText = 'Time elapsed: '+sec
+    } 
+   }, 1000)
+function reset(){
+    location.reload(true);
+}
+function pause(){
+ starting = 0
+}
