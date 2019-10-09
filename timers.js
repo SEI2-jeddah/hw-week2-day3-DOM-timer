72b88e4dd8ffa2d9822eb34e41b13fb6f9c09853
Dag <Dghistania@merrimack.edu>
2019-10-09 08:52:28 +0300

    I solved the problem

--- timers.js
+++ timers.js
@@ -1,4 +1,32 @@
 
+var timer = document.getElementById('timer');
+var start = document.getElementById('start');
+var reset = document.getElementById('reset');
+var pause = document.getElementById('pause');
+ 
+start.addEventListener("click",startPlease );
+pause.addEventListener("click",pausePlease );
+reset.addEventListener("click",resetPlease );
+
+var clockID;
+var seconds = 0;
+
+function startPlease(){
+    clockID= setInterval(function(){
+        seconds = seconds + 1  ;
+    timer.innerText = "Time elapsed: " + seconds;
+    
+}, 1000);
+}
 
 
+function pausePlease(){
+    clearInterval(clockID)
+}
+
+function resetPlease(){
+    seconds = 0;
+    timer.innerText = "Stop Watch";
+    clearInterval(clockID)
+}
 
