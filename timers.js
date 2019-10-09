fb4d8ed065768068df7285f748c3c645e2d058bf
Hisham Bin Seddeq <prog.hbs@hotmail.com>
2019-10-09 14:04:58 +0300

    finished the hw

--- timers.js
+++ timers.js
@@ -1,4 +1,18 @@
-
-
-
-
+let $timer = document.getElementById("timer");
+let interval = null; // to use clear interval
+let counter = 0;
+let startCounter = function() {
+    let counter = 0;
+    timeElpased = setInterval(counter,1000);
+    $timer.innerText = "Time Elpased "+  timeElpased;
+  }
+  function start() {
+    interval = setInterval(startCounter,1000) 
+  }
+  function clearInt() {
+    clearInterval(interval); 
+  }
+  function resetInt()
+  {
+    location.reload();
+  }
