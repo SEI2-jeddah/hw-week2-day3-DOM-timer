0564520c047b427da1ff0cf2d57c045b46b9bce3
Majd <mjalshrf@gmail.com>
2019-10-11 14:09:24 +0300

    Update timers.js

--- timers.js
+++ timers.js
@@ -1,4 +1,28 @@
+var start = document.getElementById("start");
+var timer = document.getElementById("timer");
+var reset = document.getElementById("reset");
+var pause = document.getElementById("pause");
 
+var seconds = 0;
+var timerID;
+
+start.addEventListener("press", function() {
+  timer.textContent = "Time Elapsed: " + seconds;
+  timerID = setInterval(function() {
+    seconds ++;
+    timer.textContent = "Time Elapsed: " + seconds;
+  }, 1000);
+
+});
+reset.addEventListener("press", function() {
+  clearInterval(timerID);
+  timer.textContent = "Stop Watch";
+  seconds = 0;
+});
+
+pause.addEventListener("press", function() {
+  clearInterval(timerID);
+})
 
 
 
