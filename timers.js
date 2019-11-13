b8b49782f1f620b54902869a31a826c8f545e2dd
“bosheca” <bosheca@protonmail.com>
2019-11-13 17:29:21 +0300

    Hamed Alsoubhi

--- timers.js
+++ timers.js
@@ -1,41 +1,29 @@
-
-    var reset=document.getElementById('reset');
-    reset.addEventListener('click',resetClock);
-    
-    var start=document.getElementById('start');
-    start.addEventListener('click',startClock);
-    
-    var pause=document.getElementById('pause');
-    pause.addEventListener('click',pauseClock);
-    
-    var timer=document.getElementById('timer');
-    var i=0;
-    let t;
-    function resetClock(){
-    i=0;
-    timer.innerText="Stop Watch";
+var reset = document.getElementById('reset');
+reset.addEventListener('click', resetClock);
+var start = document.getElementById('start');
+start.addEventListener('click', startClock);
+var pause = document.getElementById('pause');
+pause.addEventListener('click', pauseClock);
+var timer = document.getElementById('timer');
+var i = 0;
+let t;
+function resetClock() {
+    i = 0;
+    timer.innerText = "Stop Watch";
     clearInterval(t);
-    }
-    
-    function startClock(){
-    t=setInterval(function(){
-    timer.innerText="Time Elapsed: "+i
-    i++;
-    
-    },1000)
-    
-    }
-    
-    function pauseClock(){
+}
+function startClock() {
+    t = setInterval(function () {
+        timer.innerText = "Time Elapsed: " + i
+        i++;
+
+    }, 1000)
+
+}
+function pauseClock() {
     clearInterval(t);
-    
-    
-    }
-    
-    function updateTime(){ }
-    
-    
 
 
+}
 
-// {“liveServer.settings.port": 550 }
+function updateTime() {}
